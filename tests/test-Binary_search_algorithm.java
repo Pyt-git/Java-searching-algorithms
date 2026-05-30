0d2859c1d0da7a193bed43a7f71caf18fbd4f208
Pyt-git <weicky0804@gmail.com>
2026-05-30 11:59:59 -0400

    Update test-Binary_search_algorithm.java

--- tests/test-Binary_search_algorithm.java
+++ tests/test-Binary_search_algorithm.java
@@ -1,1 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+class BinarySearchTest {
+
+  void testFindsElementInMiddle() {
+    int [] arr = {1, 3, 5, 7, 9};
+    assertEquals(2, 
+      
