8e388f009fbc94c40f7f5704fca2b6f9c388a7f6
Pyt-git <weicky0804@gmail.com>
2026-05-30 12:16:48 -0400

    Update test-Linear_search_algorithm.java

--- tests/test-Linear_search_algorithm.java
+++ tests/test-Linear_search_algorithm.java
@@ -5,41 +5,41 @@ class LinearSearchTest {
 
   void testFindsElementInMiddle() {
     int [] arr = {1, 3, 5, 7, 9};
-    assertEquals(2, linearsearch(arr, 5)); 
+    assertEquals(2, linearSearch(arr, 5)); 
   }
 
   void testFindsElementAtStart() {
     int [] arr = {3, -6, 2, 4};
-    assertEquals(0, linearsearch(arr, 3)); 
+    assertEquals(0, linearSearch(arr, 3)); 
   }
 
   void testFindsElementAtEnd() {
     int [] arr = {2, 4, 6, 8}; 
-    assertEquals(3, linearsearch(arr, 8)); 
+    assertEquals(3, linearSearch(arr, 8)); 
   }
 
   void testFindsElementInArray() { 
     int [] arr = {-7, 0, 2, -9, 6}; 
-    assertEquals(1, linearsearch(arr, 0)); 
+    assertEquals(1, linearSearch(arr, 0)); 
   }
 
   void testElementNotFound() { 
     int [] arr = {1, 3, 5, 7, 9}; 
-    assertEquals(-1, linearsearch(arr, 11)); 
+    assertEquals(-1, linearSearch(arr, 11)); 
   }
 
   void testEmptyArray() { 
     int [] arr = {}; 
-    assertEquals(-1, binarysearch(arr, 0)); 
+    assertEquals(-1, linearSearch(arr, 0)); 
   }
 
   void testSingleElementArrayFound() {
     int [] arr = {7};
-    assertEquals(0, binarysearch(7)); 
+    assertEquals(0, linearSearch(7)); 
   }
 
   void testSingleElementArrayNotFound() {
     int [] arr = {9}; 
-    assertEquals(-1, binarysearch(4)); 
+    assertEquals(-1, linearSearch(4)); 
   }
 }
