18ae2afdbdf0f96960c7e5a9533d5478ef0adee0
Pyt-git <weicky0804@gmail.com>
2026-05-31 15:11:24 -0400

    Update test-Binary_search_algorithm.java

--- tests/test-Binary_search_algorithm.java
+++ tests/test-Binary_search_algorithm.java
@@ -3,41 +3,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTest {
 
+  @Test
   void testFindsElementInMiddle() {
     int [] arr = {1, 3, 5, 7, 9};
     assertEquals(2, binarySearch(arr, 5)); 
   }
 
+  @Test
   void testFindsElementAtStart() {
     int [] arr = {3, -6, 2, 4};
     assertEquals(0, binarySearch(arr, 3)); 
   }
 
+  @Test
   void testFindsElementAtEnd() {
     int [] arr = {2, 4, 6, 8}; 
     assertEquals(3, binarySearch(arr, 8)); 
   }
 
+  @Test
   void testFindsElementInArray() { 
     int [] arr = {-7, 0, 2, -9, 6}; 
     assertEquals(1, binarySearch(arr, 0)); 
   }
 
+  @Test
   void testElementNotFound() { 
     int [] arr = {1, 3, 5, 7, 9}; 
     assertEquals(-1, binarySearch(arr, 11)); 
   }
 
+  @Test
   void testEmptyArray() { 
     int [] arr = {}; 
     assertEquals(-1, binarySearch(arr, 0)); 
   }
 
+  @Test
   void testSingleElementArrayFound() {
     int [] arr = {7};
     assertEquals(0, binarySearch(7)); 
   }
 
+  @Test
   void testSingleElementArrayNotFound() {
     int [] arr = {9}; 
     assertEquals(-1, binarySearch(4)); 
